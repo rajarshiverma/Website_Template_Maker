c4d0c7fd3a0975a8a6325e317d63576ad1e2b08b
rajarshiverma <rajverma9719@gmail.com>
2024-12-20 08:59:46 +0530

    adding frontend files

--- test.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-api_key = "AIzaSyAaSetPbQmuQK3CMTvaVUygFXIk69oNQV8"
-genai.configure(api_key="AIzaSyAaSetPbQmuQK3CMTvaVUygFXIk69oNQV8")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("How much total tokens I have using gemini api key")
-print(response.text)
